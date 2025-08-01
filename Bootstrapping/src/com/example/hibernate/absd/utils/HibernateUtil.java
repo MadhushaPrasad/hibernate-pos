5c36d05317352ae0b799d0d716bfc5dd4913a4f9
Madhusha Prasad <madushaprasad21@gmail.com>
2025-08-01 16:59:38 +0530

    create session factory

---
+++ Bootstrapping/src/com/example/hibernate/absd/utils/HibernateUtil.java
@@ -0,0 +1,34 @@
+package com.example.hibernate.absd.utils;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.model.naming.ImplicitNamingStrategy;
+import org.hibernate.boot.model.naming.ImplicitNamingStrategyJpaCompliantImpl;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class HibernateUtil {
+
+    private static SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+
+        StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().configure().build();
+
+        Metadata metadata = new MetadataSources(serviceRegistry)
+                .getMetadataBuilder()
+                .applyImplicitNamingStrategy(ImplicitNamingStrategyJpaCompliantImpl.INSTANCE)
+                .build();
+
+        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
+
+
+        return sessionFactory;
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
+
